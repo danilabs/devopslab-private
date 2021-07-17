40912e0aef96d6e5313d785ccab4f1f097c422e8
DaniLabs <danihzt@gmail.com>
2021-07-17 10:37:40 -0700

    Movido todo a roles

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 cd ./ansible/
 
-#sleep 1
-#ansible-playbook -i hosts 00-prerequisites-on-all.yaml
-
-#sleep 10
-#ansible-playbook -i hosts 01-install-nfs-on-master.yaml
-
-#sleep 10
-#ansible-playbook -i hosts 02-mount-nfs-on-workers.yaml
-
-#sleep 10
-#ansible-playbook -i hosts 03-install-docker-k8s-on-all.yaml
+sleep 2
+ansible-playbook -i hosts 00-prerequisites-on-all.yaml
 
 sleep 10
+ansible-playbook -i hosts 01-install-nfs-server-on-master.yaml
+
+sleep 2
+ansible-playbook -i hosts 02-install-nfs-client-on-workers.yaml
+
+sleep 10
+ansible-playbook -i hosts 03-install-docker-k8s-on-all.yaml
+
+sleep 2
 ansible-playbook -i hosts 04-configure-k8s-on-master.yaml
 
-sleep 10
+sleep 2
 ansible-playbook -i hosts 05-install-calico-sdn-on-master.yaml
 
-sleep 10
+sleep 2
 ansible-playbook -i hosts 06-configure-k8s-on-workers.yaml
 
-sleep 10
-ansible-playbook -i hosts 07-install-ingress-controller-on-master.yaml
+sleep 2
+ansible-playbook -i hosts 07-install-haproxy-ingress-on-master.yaml
 
-sleep 10
+sleep 2
 ansible-playbook -i hosts 08-deploy-app-on-master.yaml
+
+#TODO: Revisar problema con el join-command (hacer el modulo de copy es mas facil y sencillo)
+#TODO: Revisar el problema con exponer el puerto de la aplicacion
