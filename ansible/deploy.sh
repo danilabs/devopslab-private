fe0946e6374d43cd6afc9f0ba89afbc2b839756e
DaniLabs <danihzt@gmail.com>
2021-07-18 03:02:16 -0700

    Refactorizado roles y cambios menores

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -7,26 +7,17 @@ ansible-playbook -i hosts 00-prerequisites-on-all.yaml
 sleep 10
 ansible-playbook -i hosts 01-install-nfs-server-on-master.yaml
 
-sleep 2
+sleep 5
 ansible-playbook -i hosts 02-install-nfs-client-on-workers.yaml
 
 sleep 10
 ansible-playbook -i hosts 03-install-docker-k8s-on-all.yaml
 
 sleep 2
-ansible-playbook -i hosts 04-configure-k8s-on-master.yaml
+ansible-playbook -i hosts 04-configure-k8s-calico-sdn-haproxy-on-master.yaml
 
 sleep 2
-ansible-playbook -i hosts 05-install-calico-sdn-on-master.yaml
+ansible-playbook -i hosts 05-configure-k8s-on-workers.yaml
 
 sleep 2
-ansible-playbook -i hosts 06-configure-k8s-on-workers.yaml
-
-sleep 2
-ansible-playbook -i hosts 07-install-haproxy-ingress-on-master.yaml
-
-sleep 2
-ansible-playbook -i hosts 08-deploy-app-on-master.yaml
-
-#TODO: Revisar problema con el join-command (hacer el modulo de copy es mas facil y sencillo)
-#TODO: Revisar el problema con exponer el puerto de la aplicacion
+ansible-playbook -i hosts 06-deploy-app-on-master.yaml
